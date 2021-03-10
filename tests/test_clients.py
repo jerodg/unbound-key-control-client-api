6a17dd47a2aa1adfa10d1190753832e107d6466a
Jerod Gawne <jerodgawne@gmail.com>
2021-03-10 16:45:15 -0700

    [refac](test_clients) Finalizing Logic
    ~ Refactor tests to handle new base logic

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -23,12 +23,15 @@ from os import getenv
 import pytest
 from base_client_api.models.results import Results
 from base_client_api.utils import bprint, tprint
+from loguru import logger
 from pydantic import ValidationError
 from rich import print
 
 from unbound_key_control_client_api.models.clients import (ClientCreateOne, ClientRefreshActivationCode, ClientsListAll,
                                                            RefreshedCertificateClient)
 from unbound_key_control_client_api.ukc_client import UkcClient
+
+logger.enable('base_client_api')
 
 
 @pytest.mark.asyncio
@@ -37,7 +40,9 @@ async def test_clients_list_all():
     bprint('Test: Clients List All', 'top')
 
     async with UkcClient(cfg=f'{getenv("CFG_HOME")}/unbound_snd.toml') as ukc:
-        results = await ukc.make_request(models=ClientsListAll())
+        m = ClientsListAll(limit=25, skip=3, partitionId='sandbox')
+        print(m.parameters)
+        results = await ukc.make_request(models=m)
 
         assert type(results) is Results
         assert results.success is not None
@@ -54,33 +59,21 @@ async def test_client_create_one():
     bprint('Test: Clients Create One', 'top')
 
     async with UkcClient(cfg=f'{getenv("CFG_HOME")}/unbound_snd.toml') as ukc:
-        # results = await ukc.make_request(models=ClientCreateOne(name=f'test_client_api',
-        #                                                         check_ip=False,
-        #                                                         allow_nat=False,
-        #                                                         expiration=333333,
-        #                                                         activation_code_validity=333333,
-        #                                                         is_template=False,
-        #                                                         activation_code_length=33,
-        #                                                         ip_range='0.0.0.0/0',
-        #                                                         certificate_expiration=333333))
+        results = await ukc.make_request(models=ClientCreateOne(name=f'test_client_api',
+                                                                checkIp=False,
+                                                                allowNat=False,
+                                                                expiration=333333,
+                                                                activationCodeValidity=333333,
+                                                                isTemplate=False,
+                                                                activationCodeLength=33,
+                                                                ipRange='0.0.0.0/0',
+                                                                certificateExpiration=333333))
 
-        m = ClientCreateOne(name=f'test_client_api',
-                            check_ip=False,
-                            allow_nat=False,
-                            expiration=333333,
-                            activation_code_validity=333333,
-                            is_template=False,
-                            activation_code_length=33,
-                            ip_range='0.0.0.0/0',
-                            certificate_expiration=333333)
-        print(m)
-        print(m.body)
+        assert type(results) is Results
+        assert results.success is not None
+        assert not results.failure
 
-        # assert type(results) is Results
-        # assert results.success is not None
-        # assert not results.failure
-        #
-        # tprint(results)
+        tprint(results)
 
     bprint(f'Completed in {(time.perf_counter() - ts):f} seconds.', 'bottom')
 
@@ -93,14 +86,14 @@ async def test_client_create_one_check_validation():
     async with UkcClient(cfg=f'{getenv("CFG_HOME")}/unbound_snd.toml') as ukc:
         try:
             await ukc.make_request(models=ClientCreateOne(name=f'test_client_api',
-                                                          check_ip=False,
-                                                          allow_nat=False,
+                                                          checkIp=False,
+                                                          allowNat=False,
                                                           expiration=3333333333,
-                                                          activation_code_validity=333333,
-                                                          is_template=False,
-                                                          activation_code_length=33,
-                                                          ip_range='0.0.0.0/0',
-                                                          certificate_expiration=333333))
+                                                          activationCodeValidity=333333,
+                                                          isTemplate=False,
+                                                          activationCodeLength=33,
+                                                          ipRange='0.0.0.0/0',
+                                                          certificateExpiration=333333))
         except ValidationError as ve:
             bprint(str(ve))
             # The expected success criteria for this test is for this error to be raised.
@@ -114,29 +107,24 @@ async def test_client_refresh_activation_code():
     bprint('Test: Client Refresh Activation Code', 'top')
 
     async with UkcClient(cfg=f'{getenv("CFG_HOME")}/unbound_snd.toml') as ukc:
-        p = RefreshedCertificateClient(certificateExpiration=3333,
-                                       activationCodeValidity=3333,
-                                       activationCodeLength=15,
-                                       ipRange='0.0.0.0/8')
-        # print(p)
-        # print('p:', p.json(by_alias=True))
-        # print('p2:', p.dict(by_alias=True))
+        m = ClientRefreshActivationCode(
+                clientId='test_client_api',
+                body=RefreshedCertificateClient(
+                        certificateExpiration=3333,
+                        activationCodeValidity=3333,
+                        activationCodeLength=15,
+                        ipRange='0.0.0.0/8'))
 
-        m = ClientRefreshActivationCode(clientId='test_client_api',
-                                        payload=p)
-        # # m = ClientRefreshActivationCode(client_id='test_client_api')
-        # m = ClientRefreshActivationCode(clientId='test_client_api')
-        print(m.body)
-        print(m.endpoint)
-        print(m.dict())
-        # print(m.payload)
+        print(m)
+        print(m.parameters)
+        print(m.json_body)
 
-        # results = await ukc.make_request(models=m)
-        #
-        # assert type(results) is Results
-        # assert results.success is not None
-        # assert not results.failure
-        #
-        # tprint(results)
+        results = await ukc.make_request(models=m)
+
+        assert type(results) is Results
+        assert results.success is not None
+        assert not results.failure
+
+        tprint(results)
 
     bprint(f'Completed in {(time.perf_counter() - ts):f} seconds.', 'bottom')
